5a0032108f02be28222513863f2a92ac09bc202e
DSlundro <dragan.savic95@outlook.it>
2022-09-17 23:51:21 +0200

    handling from submission

--- src/components/Helpers.js
+++ src/components/Helpers.js
@@ -3,3 +3,4 @@ export const getMonth = date => date.toLocaleString('it-IT', {month: 'long'});
 export const getYear = date => date.getFullYear();
 export const getDay = date => date.toLocaleString('it-IT', {day: '2-digit'});
 
+
